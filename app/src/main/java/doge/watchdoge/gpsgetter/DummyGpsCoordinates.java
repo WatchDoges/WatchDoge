540e6a3388f113b21fa0f47657f94ae82d0f6afa
Patrik <hillner.patrik@gmail.com>
2016-10-13 17:49:29 +0300

    Fix DummyGpsCoordinates constructor

--- app/src/main/java/doge/watchdoge/gpsgetter/DummyGpsCoordinates.java
+++ app/src/main/java/doge/watchdoge/gpsgetter/DummyGpsCoordinates.java
@@ -8,7 +8,7 @@ public class DummyGpsCoordinates {
     private float gpsAccuracy = 0;
     private long gpsAge = 0;
 
-    public void GpsCoordinates(Context context) {
+    public void DummyGpsCoordinates(Context context) {
         gpsCoords = new Pair<Double, Double>(60.462518, 22.288918);
         gpsAccuracy = (float) 5.0;
         gpsAge = (long) 2;
