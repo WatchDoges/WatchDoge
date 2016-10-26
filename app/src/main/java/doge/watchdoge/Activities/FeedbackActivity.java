14c8b2f4fab0b6dcd7d21682a9b0bbea51b87adc
GorbadPS3 <miroeklu@gmail.com>
2016-10-26 12:23:48 +0300

    Added click listeners to all existing buttons
    Also added placeholder functions for said functions in their
    corresponding activities. Also added a new button.
    closes #43

--- app/src/main/java/doge/watchdoge/Activities/FeedbackActivity.java
+++ app/src/main/java/doge/watchdoge/Activities/FeedbackActivity.java
@@ -2,6 +2,7 @@ package doge.watchdoge.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import doge.watchdoge.R;
 
@@ -11,5 +12,18 @@ public class FeedbackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feedback_activity_layout);
+    }
+
+    public void resendButtonClick(View v){
+        //PLACEHOLDER
+    }
+
+
+    public void closeButtonClick(View v){
+        //PLACEHOLDER
+    }
+
+    public void newReportButtonClick(View v){
+        //PLACEHOLDER
     }
 }
