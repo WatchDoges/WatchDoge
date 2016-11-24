79de5ca7812ff389388233c8e034987351399533
GorbadPS3 <miroeklu@gmail.com>
2016-11-24 14:49:28 +0200

    Updated album view. A lot.
    Lots of added code for album. Now shows the images correctly. Can swipe
    left/right to see other images.
    
    Updated MainActivity to not include the help-fields, since they looked
    clunky and awkward. Help feature will be implemented in another way.
    
    Delete button for pictures still in the to-do list.

--- app/src/main/java/doge/watchdoge/activities/AlbumActivity.java
+++ app/src/main/java/doge/watchdoge/activities/AlbumActivity.java
@@ -1,28 +1,40 @@
 package doge.watchdoge.activities;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.view.GestureDetectorCompat;
-import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.ViewSwitcher;
 
+import java.io.IOException;
 import doge.watchdoge.R;
+import doge.watchdoge.imagehandlers.ImageHandlers;
 
 public class AlbumActivity extends AppCompatActivity
         implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
 
+    private static final int RIGHT = 1;
+    private static final int LEFT = -1;
+    private static final int DEFAULT = 0;
     private static final String DEBUG_TAG = "Gestures";
     private GestureDetectorCompat mDetector;
+    private ImageSwitcher imageSwitcher;
+    private int state = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.album_activity_layout);
+        initImageSwitcher();
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.drawable.ic_menu_tys);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
@@ -35,21 +47,30 @@ public class AlbumActivity extends AppCompatActivity
         mDetector.setOnDoubleTapListener(this);
     }
 
+    private void initImageSwitcher(){
+        System.out.println("Initing image switcher.");
+        imageSwitcher = (ImageSwitcher)findViewById(R.id.imageSwitcher1);
+        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            public View makeView() {
+                ImageView myView = new ImageView(getApplicationContext());
+                return myView;
+            }
+        });
+        Uri req = getRequestedUri(DEFAULT);
+        updateImageView(req);
+    }
 
     public void backToMainButtonClick(View v){
+        state = 0;
         finish();
     }
 
     @Override
-    public boolean onFling(MotionEvent event1, MotionEvent event2,
-                           float velocityX, float velocityY) {
-        if(velocityX > 50){
-            Toast t = Toast.makeText(this.getApplicationContext(), "Flinging right!", Toast.LENGTH_SHORT);
-            t.show();
-        } else if (velocityX < -50){
-            Toast t = Toast.makeText(this.getApplicationContext(), "Flinging left!", Toast.LENGTH_SHORT);
-            t.show();
-        }
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velX, float velY) {
+        Uri requestedUri = null;
+        if(velX > 50) requestedUri = getRequestedUri(LEFT); //direction is right, going left.
+        else if (velX < -50) requestedUri = getRequestedUri(RIGHT); //direction is left, going right.
+        updateImageView(requestedUri);
         return true;
     }
 
@@ -84,4 +105,54 @@ public class AlbumActivity extends AppCompatActivity
     @Override
     public boolean onSingleTapConfirmed(MotionEvent event) { return true; }
 
+    private Uri getRequestedUri(int direction){
+        System.out.println("Requesting uri");
+        try {
+            Object[] uris = (MainActivity.uris.values()).toArray();
+            int length = uris.length;
+            if(length==0) return null;
+            else {
+                int targetIndex = newState(direction, length);
+                Uri target = (Uri) uris[targetIndex];
+                System.out.println("Returning actual target");
+                return target;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Returning null target");
+            return null;
+        }
+    }
+
+    private int newState(int direction, int maxLength){
+        System.out.println("Setting state. MaxLength: " + maxLength);
+        if(maxLength==0) return (updateState(0));
+        else {
+            if(direction==RIGHT){
+                if(state==(maxLength-1)) return (updateState(0));
+                else return (updateState(state + 1));
+            } else if(direction==LEFT){//going left
+                if(state==0) return (updateState(maxLength-1));
+                else return (updateState(state - 1));
+            } else {
+                return state;
+            }
+        }
+    }
+
+    private int updateState(int newState){
+        state = newState;
+        return state;
+    }
+
+    private void updateImageView(Uri uri){
+        if(uri!=null) imageSwitcher.setImageURI(uri);
+    }
+
+    public void deletePictureClick(View v){
+        //Uri uri = getRequestedUri(DEFAULT);
+        //ImageHandlers.deleteFileByUri(uri);
+        Toast t = Toast.makeText(this.getApplicationContext(), "To do", Toast.LENGTH_LONG);
+        t.show();
+    }
 }
